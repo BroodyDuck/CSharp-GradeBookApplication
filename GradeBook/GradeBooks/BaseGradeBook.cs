acf8803ffdff7c13a730e0626c432d5a682e7257
BroodyDuck <wdj1994@live.co.uk>
2018-02-22 15:31:46 +0000

    test

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -11,10 +11,11 @@ namespace GradeBook.GradeBooks
 {
     public abstract class BaseGradeBook
     {
-        public bool isWeighted { get; set; }
+        
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool isWeighted { get; set; }
 
         public BaseGradeBook(string name, bool isWeighted)
         {
